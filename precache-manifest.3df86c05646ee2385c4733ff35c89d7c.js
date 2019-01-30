340412c74c4fbbe277114ec3fd30f63dccb669b8
Hy Huynh <hyxhuynh@gmail.com>
2019-01-30 03:51:56 -0800

    Updates

---
+++ precache-manifest.3df86c05646ee2385c4733ff35c89d7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b2f870229b4e20beca5",
+    "url": "/ClickyGame/static/css/main.c31446db.chunk.css"
+  },
+  {
+    "revision": "6b2f870229b4e20beca5",
+    "url": "/ClickyGame/static/js/main.6b2f8702.chunk.js"
+  },
+  {
+    "revision": "85d3f30ffe9b46d07e5c",
+    "url": "/ClickyGame/static/js/1.85d3f30f.chunk.js"
+  },
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "e36e67fde851001c3414cf38212b71e0",
+    "url": "/ClickyGame/index.html"
+  }
+];
